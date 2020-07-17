65c87bb9197eb869ae2646cc999082e3b8a7a7b8
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 13:17:06 -0400

    Included function to check database

--- public/db.js
+++ public/db.js
@@ -24,4 +24,30 @@ function saveRecord(record) {
     const store = transaction.objectStore("pending");
   
     store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
   }
