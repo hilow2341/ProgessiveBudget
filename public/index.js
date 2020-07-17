1143afa37562ec5ad83379be121971604b935c7c
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 12:59:21 -0400

    Refactored javascript for reliablity

--- public/index.js
+++ public/index.js
@@ -144,10 +144,26 @@ function sendTransaction(isAdding) {
   });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(true);
-};
+});
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").addEventListener("click", function(event) {
+  event.preventDefault();
   sendTransaction(false);
-};
+});
+
+document.querySelector("#del-btn").addEventListener("click", function(event) {
+  event.preventDefault();
+  deletePending();
+});
+
+
+// document.querySelector("#add-btn").onclick = function() {
+//   sendTransaction(true);
+// };
+
+// document.querySelector("#sub-btn").onclick = function() {
+//   sendTransaction(false);
+// };
