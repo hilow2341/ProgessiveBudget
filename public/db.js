4a6dec46ebd585a071536fe46569b11ddd988d3a
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-17 13:06:00 -0400

    Added database requests

---
+++ public/db.js
@@ -0,0 +1,20 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log("Woops! " + event.target.errorCode);
+};
